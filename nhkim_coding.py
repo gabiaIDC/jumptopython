5ec0a70e6be1fafa8dc8a66574eb017cf530583a
Nathan Kim <keneich2@gmail.com>
2016-12-09 18:29:07 +0900

    memo project add

--- nhkim_coding.py
+++ nhkim_coding.py
@@ -2,9 +2,32 @@
     2016-12-09
     김남효 jumptopython 코딩 연습
 '''
-
+'''
 print('hello world')
 
 a=10
 b=20
 print(a+b)
+
+
+# 집한 자료형 : 중복 허용 x, 순서가 없다.
+s1 = set([1,2,3])
+print(s1)
+
+s2 = set("hello")
+print(s2)
+
+
+
+s1 = set([1,2,3,4,5,6])
+s2 = set([4,5,6,7,8,9])
+
+print(s1&s2)
+print(s1|s2)
+print(s1.union(s2))
+
+s1=set([1,2,3])
+s2=s1.remove(2)
+print(s2)
+'''
+
